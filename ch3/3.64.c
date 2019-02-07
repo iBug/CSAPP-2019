524b776dbb2b16aa41ba926e1c2e76f86e9ee9dd
iBug <7273074+iBug@users.noreply.github.com>
2019-02-08 00:16:53 +0800

    Add 3.64

---
+++ ch3/3.64.c
@@ -0,0 +1,13 @@
+// File: 3.64.c
+// Author: iBug
+
+#define R 7
+#define S 5
+#define T 13
+
+long A[R][S][T];
+
+long store_ele(long i, long j, long k, long* dest) {
+    *dest = A[i][j][k];
+    return sizeof(A);
+}
