9db7b4a82c556cdc248df61f30ec98a816eea56f
iBug <7273074+iBug@users.noreply.github.com>
2019-02-09 12:01:53 +0800

    add 3.70

---
+++ ch3/3.70.c
@@ -0,0 +1,17 @@
+// File: 3.70.c
+// Author: iBug
+
+union ele {
+    struct {
+        long *p;
+        long y;
+    } e1;
+    struct {
+        long x;
+        union ele *next;
+    } e2;
+};
+
+void proc(union ele *up) {
+    up->e2.x = *(up->e2.next->e1.p) - up->e2.next->e1.y;
+}
